4b4ad7c9cf8f35fe809857d079299edfebfa8c03
afshan ahmed khan <afshanahmeda2k@gmail.com>
2023-11-07 16:22:48 +0530

    Bugs in rendering

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const cors =require("cors");
 const dotenv =require('dotenv');
 const {router}=require('./controllers/router.js')
+const bodyParser = require('body-parser');
 dotenv.config()
 
 app.use(function(req, res, next) {
@@ -15,8 +16,11 @@ app.use(function(req, res, next) {
 
 console.log("Reached here");
 
+app.set('view engine', 'ejs');
 app.use(cors())
-app.use(express.json())
+// app.use(express.json())
+app.use(bodyParser.json({limit:'50mb'}));
+app.use(bodyParser.json({limit:'50mb',extended:true}));
 app.use(router)
 app.listen(process.env.PORT,()=>{
     console.log("server is running on "+process.env.PORT)
--- controllers/router.js
+++ controllers/router.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { firestore } = require('firebase-admin');
 const router = express.Router()
+const fs = require('fs');
+const path = require('path')
 
 var admin = require("firebase-admin");
 
 var serviceAccount = require("../admin.json");
+const { getHtmlCss } = require('../utils/editor');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -13,14 +16,9 @@ admin.initializeApp({
 const db = firestore()
 
 router.post("/venue/owner/web", async (req, res) => {
-    console.log(req.body);
     const webDb = db.collection("web");
-    //    const snapshot = await webDb.get();
-    //snapshot.forEach(doc => {
-    //  console.log(doc.id, '=>', JSON.stringify(doc.data()));
-    //});
-    const data = req.body
-    const response = await webDb.doc('1234564125').set(data);
+    const data = JSON.stringify(req.body)
+    const response = await webDb.doc('1234564125').set({ editorData: data });
     console.log(response)
     res.json({ message: "request received" })
 })
@@ -28,16 +26,41 @@ router.post("/venue/owner/web", async (req, res) => {
 router.get("/venue/owner/web", async (req, res) => {
     const webDb = db.collection("web");
     const docRef = webDb.doc('1234564125');
-    
+
     const doc = await docRef.get();
 
     if (!doc.exists) {
         res.status(404).json({ message: "Document not found" });
     } else {
         const data = doc.data();
-        res.json(data);
+        res.json(JSON.parse(data.editorData));
     }
 });
+
+let template = ' <!DOCTYPE html> <html lang="en"> <head> <meta charset="utf-8"> style </head> body </html> '
+router.get('/render', async (req, res) => {
+    const webDb = db.collection("web");
+    const docRef = webDb.doc('1234564125');
+
+    const doc = await docRef.get();
+
+    if (!doc.exists) {
+        res.status(404).json({ message: "Document not found" });
+    } else {
+        const data = doc.data();
+        let htmlCss = getHtmlCss(JSON.parse(data.editorData))
+        console.log(htmlCss)
+        fs.writeFile('./main.html', htmlCss[0].html, (error) => { console.log("error is - ", error) });
+        // fs.writeFile('./main.css', htmlCss[0].css, (error) => { console.log("error is - ", error) });
+        let style = `<style>${htmlCss[0].css}</style>`
+        // let body = `${htmlCss[0].html}`
+        template = template.replace('style', style)
+        template = template.replace('body', htmlCss[0].html)
+        fs.writeFile('./index.html', template, (error) => { console.log("error is - ", error) })
+        res.send(template);
+    }
+})
+
 
 module.exports = { router };
 
