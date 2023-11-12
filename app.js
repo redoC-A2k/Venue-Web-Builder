946785fe80c20fc2b8a78024976a0d8bbfade545
afshan ahmed khan <afshanahmeda2k@gmail.com>
2023-11-12 13:14:42 +0530

    Fixed rendering bugs

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    console.log("cors");
     next();
   });
 
--- controllers/router.js
+++ controllers/router.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const { firestore } = require('firebase-admin');
+// const gjs_
 const router = express.Router()
-const fs = require('fs');
-const path = require('path')
 
 var admin = require("firebase-admin");
 
 var serviceAccount = require("../admin.json");
-const { getHtmlCss } = require('../utils/editor');
+// const { getHtmlCss } = require('../utils/editor.mjs');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -41,7 +40,7 @@ router.get("/venue/owner/web", async (req, res) => {
 let part1 = `<!DOCTYPE html>
 <html>
 <head>
-<title>Title</title>`
+<title>Grapesjs page</title>`
 let part2 = `</head>`
 let part3 = `</html>`
 router.get('/venue/render', async (req, res) => {
@@ -54,10 +53,15 @@ router.get('/venue/render', async (req, res) => {
         res.status(404).json({ message: "Document not found" });
     } else {
         const data = doc.data();
-        let htmlCss = getHtmlCss(JSON.parse(data.editorData))
-        let template = part1+`<style>${htmlCss[0].css}</style>`+part2+htmlCss[0].html+part3
-        console.log("Got template")
-        res.send(template);
+        Promise.all([
+            import('../utils/editor.mjs')
+        ]).then((arg) => {
+            let getHtmlCss = arg[0].default
+            let htmlCss = getHtmlCss(JSON.parse(data.editorData))
+            let template = part1 + `<style>${htmlCss[0].css}</style>` + part2 + htmlCss[0].html + part3
+            console.log("Got template")
+            res.send(template);
+        })
     }
 })
 
