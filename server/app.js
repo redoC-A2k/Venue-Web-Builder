53ca87c413fbe18798a1d89f2c7a336cb6c1afda
afshan ahmed khan <afshanahmeda2k@gmail.com>
2024-02-08 08:56:03 +0530

    Allow multiple cors origins

--- server/app.js
+++ server/app.js
@@ -5,13 +5,15 @@ const router = require('./router.js')
 const bodyParser = require('body-parser');
 dotenv.config()
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
-  // res.header("Access-Control-Allow-Origin", process.env.BUILD_CLIENT_URL);
+let allowedOrigins = [process.env.CLIENT_URL, 'http://localhost:8080', 'http://localhost:5500']
+app.all('*', function (req, res, next) {
+  const origin = allowedOrigins.includes(req.headers.origin) ? req.headers.origin : allowedOrigins[0];
+  res.header("Access-Control-Allow-Origin", origin);
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, authorization, Accept, Authorization");
   next();
-});
+})
+
 // app.use(express.json())
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb', extended: true }));
